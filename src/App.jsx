80db27a4c7aba72a8ba5675ac4f2066a16968ee2
Hadari Sas <hadarisas@Hadaris-MacBook-Pro.local>
2024-12-09 15:31:48 +0100

    project init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div class="min-h-screen bg-gray-100">
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="px-4 py-6 sm:px-0">
+          <div class="bg-white rounded-lg shadow p-6">
+            <p class="text-center text-gray-600">
+              ChatBot App Using META AI Model.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
